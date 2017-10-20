fea4112853780216d67393715b8bafacb09db807
Liao <161250068@smail.nju.edu.cn>
2017-10-20 09:34:02 +0800

    对财务人员部分接口名做了部分修改

---
+++ Client/src/nju/lighting/businesslogicservice/documentblservice/AccountIODocBLService.java
@@ -0,0 +1,10 @@
+package nju.lighting.businesslogicservice.documentblservice;
+
+/**
+ * Created on 2017/10/19.
+ * Description:
+ * @author Liao
+ */
+public interface AccountIODocBLService {
+
+}
---
+++ Client/src/nju/lighting/businesslogicservice/documentblservice/AccountIODocBLService_Driver.java
@@ -0,0 +1,9 @@
+package nju.lighting.businesslogicservice.documentblservice;
+
+/**
+ * Created on 2017/10/19.
+ * Description:
+ * @author Liao
+ */
+public class AccountIODocBLService_Driver {
+}

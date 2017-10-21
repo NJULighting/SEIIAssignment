ce92cf6b30df7a141bcd49fddb487b6d144737ef
Chen Junyu <924342513@qq.com>
2017-10-21 13:33:16 +0800

    更新总经理VO部分

---
+++ Server/src/nju/lighting/blservice/PromotionBLService.java
@@ -0,0 +1,23 @@
+package nju.lighting.blservice;
+
+import com.sun.org.apache.regexp.internal.RE;
+import nju.lighting.vo.PromotionVO;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+/**
+ * Created on 2017/10/21.
+ * Description
+ *
+ * @author 陈俊宇
+ */
+public interface PromotionBLService {
+    ArrayList<PromotionVO> getPromotionList () throws RemoteException;
+
+    boolean newAndSave (PromotionVO vo) throws RemoteException;
+
+    boolean modifyAndSave(PromotionVO vo) throws RemoteException;
+
+    boolean delete(PromotionVO vo) throws RemoteException;
+}

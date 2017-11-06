fd6afdda09712e077bcf3b5cb904e2662407fe62
iznauy <161250220@smail.nju.edu.cn>
2017-11-06 19:15:03 +0800

    找回丢失的commodity

---
+++ Server/src/nju/lighting/po/commodity/CommodityCategoryPO.java
@@ -0,0 +1,13 @@
+package nju.lighting.po.commodity;
+
+import java.util.ArrayList;
+
+public class CommodityCategoryPO {
+
+    private int id;
+
+    private String name;
+
+    private int upperCategoryId;
+
+}

c09630386a318f944fe528d9af785791aec01801
iznauy <161250220@smail.nju.edu.cn>
2017-11-07 19:28:24 +0800

    添加/修改库存相关VO对象

--- Server/src/nju/lighting/vo/repository/RepositoryChangeVO.java
+++ Server/src/nju/lighting/vo/repository/RepositoryChangeVO.java
@@ -1,4 +1,67 @@
 package nju.lighting.vo.repository;
 
+import shared.RepositoryChangeType;
+
+import java.util.Date;
+
 public class RepositoryChangeVO {
+
+    private String commodityId;
+
+    private RepositoryChangeType type;
+
+    private int count;
+
+    private double amount;
+
+    private Date date;
+
+    public RepositoryChangeVO(String commodityId, RepositoryChangeType type, int count, double amount, Date date) {
+        this.commodityId = commodityId;
+        this.type = type;
+        this.count = count;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getCommodityId() {
+        return commodityId;
+    }
+
+    public void setCommodityId(String commodityId) {
+        this.commodityId = commodityId;
+    }
+
+    public RepositoryChangeType getType() {
+        return type;
+    }
+
+    public void setType(RepositoryChangeType type) {
+        this.type = type;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
--- Server/src/nju/lighting/vo/repository/RepositoryTableVO.java
+++ Server/src/nju/lighting/vo/repository/RepositoryTableVO.java
@@ -1,4 +1,20 @@
 package nju.lighting.vo.repository;
 
+import java.util.ArrayList;
+
 public class RepositoryTableVO {
+
+    private ArrayList<RepositoryTableItemVO> repositoryTableItemVOS;
+
+    public RepositoryTableVO(ArrayList<RepositoryTableItemVO> repositoryTableItemVOS) {
+        this.repositoryTableItemVOS = repositoryTableItemVOS;
+    }
+
+    public ArrayList<RepositoryTableItemVO> getRepositoryTableItemVOS() {
+        return repositoryTableItemVOS;
+    }
+
+    public void setRepositoryTableItemVOS(ArrayList<RepositoryTableItemVO> repositoryTableItemVOS) {
+        this.repositoryTableItemVOS = repositoryTableItemVOS;
+    }
 }

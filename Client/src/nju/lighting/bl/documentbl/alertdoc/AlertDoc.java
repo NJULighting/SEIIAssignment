28c9f79ce6779c73d0f5919b1b1698a0dfb522bc
iznauy <161250220@smail.nju.edu.cn>
2017-11-14 15:07:45 +0800

    修改alertdoc lossandgaindoc bl层代码

---
+++ Client/src/nju/lighting/bl/documentbl/alertdoc/AlertDoc.java
@@ -0,0 +1,49 @@
+package nju.lighting.bl.documentbl.alertdoc;
+
+import nju.lighting.bl.documentbl.Doc;
+import nju.lighting.po.doc.DocPO;
+import nju.lighting.vo.DocVO;
+import nju.lighting.vo.doc.alertdoc.AlertDocVO;
+import shared.DocType;
+import shared.ResultMessage;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created on 2017/11/7.
+ * Description: 处理库存报警单相关业务
+ * @author iznauy
+ */
+public class AlertDoc extends Doc {
+
+    private ArrayList<AlertDocItem> items;
+
+    public ArrayList<AlertDocItem> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<AlertDocItem> items) {
+        this.items = items;
+    }
+
+    public AlertDoc(String id, DocType docType, String userId, Date time, ArrayList<AlertDocItem> items) {
+        super(id, docType, userId, time);
+        this.items = items;
+    }
+
+    @Override
+    public void approve() {
+
+    }
+
+    @Override
+    public DocVO createVO() {
+        return null;
+    }
+
+    @Override
+    public DocPO createPO() {
+        return null;
+    }
+}
---
+++ Client/src/nju/lighting/bl/documentbl/lossandgaindoc/LossAndGainDoc.java
@@ -0,0 +1,41 @@
+package nju.lighting.bl.documentbl.lossandgaindoc;
+
+import nju.lighting.bl.documentbl.Doc;
+import nju.lighting.po.doc.DocPO;
+import nju.lighting.vo.DocVO;
+import nju.lighting.vo.doc.lossandgaindoc.LossAndGainDocVO;
+import shared.DocType;
+import shared.ResultMessage;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created on 2017/11/7.
+ * Description: 处理库存报损报溢相关业务
+ * @author iznauy
+ */
+public class LossAndGainDoc extends Doc {
+
+    private ArrayList<LossAndGainDocItem> items;
+
+    public LossAndGainDoc(String id, DocType docType, String userId, Date time, ArrayList<LossAndGainDocItem> items) {
+        super(id, docType, userId, time);
+        this.items = items;
+    }
+
+    @Override
+    public void approve() {
+
+    }
+
+    @Override
+    public DocVO createVO() {
+        return null;
+    }
+
+    @Override
+    public DocPO createPO() {
+        return null;
+    }
+}

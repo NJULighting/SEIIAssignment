3bab377f5d1cd83a458b24e4c08e855205941546
Liao <161250068@smail.nju.edu.cn>
2017-11-26 16:17:38 +0800

    解决冲突

---
+++ test/java/nju/lighting/bl/accountbl/AccountManagerTest.java
@@ -0,0 +1,55 @@
+package test.nju.lighting.bl.accountbl; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* AccountManager Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Ê®Ò»ÔÂ 22, 2017</pre> 
+* @version 1.0 
+*/ 
+public class AccountManagerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getAccountManager() 
+* 
+*/ 
+@Test
+public void testGetAccountManager() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addAccount(String id, String name, double amount) 
+* 
+*/ 
+@Test
+public void testAddAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getAccount(String id) 
+* 
+*/ 
+@Test
+public void testGetAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 

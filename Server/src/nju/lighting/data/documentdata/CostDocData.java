59fae716cf768b3b8a9931687ca47f7b85cc2d7b
iznauy <161250220@smail.nju.edu.cn>
2017-11-28 16:21:39 +0800

    重构单据模块设计

--- Server/src/nju/lighting/data/documentdata/CostDocData.java
+++ Server/src/nju/lighting/data/documentdata/CostDocData.java
@@ -1,10 +1,23 @@
 package nju.lighting.data.documentdata;
 
+import nju.lighting.po.doc.costdoc.CostDocPO;
+
 /**
  * Created on 2017/11/28.
  * Description:
  *
  * @author iznauy
  */
-public class CostDocData {
+public class CostDocData extends AbstractDocData<CostDocPO> {
+
+    private String docName = "XJFYD";
+
+    public CostDocData() {
+        super(CostDocData.class);
+    }
+
+    @Override
+    protected String getDocName() {
+        return docName;
+    }
 }

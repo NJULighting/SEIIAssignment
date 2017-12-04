973db9bd1a8b8cda099829f9d53c42a654ecab6e
Liao <161250068@smail.nju.edu.cn>
2017-12-04 20:40:03 +0800

    修改UserChangeInfo，并在类的javadoc中添加使用说明

--- Client/src/shared/UserChangeInfo.java
+++ Client/src/shared/UserChangeInfo.java
@@ -6,10 +6,16 @@ import nju.lighting.po.user.UserPO;
 
 import javax.naming.NamingException;
 import java.rmi.RemoteException;
+import java.util.function.Consumer;
 
 /**
  * Created on 2017/12/2.
- * Description:
+ * Description: Store change information for a user<br>
+ * Sample code:<br>
+ * <code>UserChangeInfo.Builder builder = new UserChangeInfo.Builder(userID);</code><br>
+ * <code>builder.rename(name).changePassword(password).changeIdentity(identity).changeAuthority(authority)</code><br>
+ * <code>UserChangeInfo info = builder.build()</code><br>
+ * <b>注：可以不用if或者switch，直接调用全部方法，哪怕该属性没有修改,该方法会自动跳过没有修改的属性</b><br>
  * @author Liao
  */
 public class UserChangeInfo {
@@ -52,24 +58,33 @@ public class UserChangeInfo {
         }
 
         public Builder rename(String name) {
+            if (this.name.equals("name")) {
+                return this;
+            }
             this.name = name;
             description += "改名为 " + name + ";";
             return this;
         }
 
         public Builder changePassword(String password) {
+            if (this.name.equals(password))
+                return this;
             this.password = password;
             description += "改密码为 " + password + ";";
             return this;
         }
 
         public Builder changeIdentity(Identity identity) {
+            if (this.identity == identity)
+                return this;
             this.identity = identity;
             description += "改身份为 " + identity + ";";
             return this;
         }
 
         public Builder changeAuthorized(boolean authorized) {
+            if (authorized == this.authorized)
+                return this;
             this.authorized = authorized;
             description += "改权限为 " + authorized + ";";
             return this;

1b6dd1cd2ab80c80759ba24d638f9ed45b2dc038
Liao <161250068@smail.nju.edu.cn>
2017-12-13 23:21:37 +0800

    添加辅助用的函数式接口 DataServiceBiFunction DataServiceSupplier

---
+++ Client/src/nju/lighting/bl/repositorybl/RepositoryInfoImpl.java
@@ -0,0 +1,9 @@
+package nju.lighting.bl.repositorybl;
+
+/**
+ * Created on 2017/12/13.
+ * Description:
+ * @author Liao
+ */
+public class RepositoryInfoImpl {
+}
---
+++ Client/src/nju/lighting/bl/utils/DataServiceBiFunction.java
@@ -0,0 +1,9 @@
+package nju.lighting.bl.utils;
+
+/**
+ * Created on 2017/12/13.
+ * Description:
+ * @author Liao
+ */
+public interface DataServiceBiFunction {
+}

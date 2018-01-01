524f3801e242126e7d03e1856ceea0629678e1a5
Chen Junyu <924342513@qq.com>
2018-01-01 21:25:16 +0800

    遗留问题

--- Client/src/nju/lighting/presentation/commodityui/Predicate.java
+++ Client/src/nju/lighting/presentation/commodityui/Predicate.java
@@ -1,10 +1,15 @@
 package nju.lighting.presentation.commodityui;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import nju.lighting.presentation.factory.CommodityBLServiceFactory;
 import nju.lighting.vo.commodity.BasicCommodityItemVO;
 import nju.lighting.vo.commodity.CommodityItemVO;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created on 2018/1/1.
@@ -18,6 +23,9 @@ public class Predicate {
     HBox imageContainer;
 
     void init(CommodityItemVO commodityItemVO){
-        CommodityBLServiceFactory.getCommodityBLService().getTrend(commodityItemVO.getId());
+
+        imageContainer.getChildren().add(new ImageView("file:C:/Users/92434/Documents/SEIIAssignment/python/analysis/image/"+commodityItemVO.getId()+".jpg"));
+
+
     }
 }

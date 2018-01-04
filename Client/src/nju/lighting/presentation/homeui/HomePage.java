b950534e8abda1839f47e02282589605aff63e99
Chen Junyu <924342513@qq.com>
2018-01-04 22:31:42 +0800

    merge

--- Client/src/nju/lighting/presentation/homeui/HomePage.java
+++ Client/src/nju/lighting/presentation/homeui/HomePage.java
@@ -1,10 +1,29 @@
 package nju.lighting.presentation.homeui;
 
+import com.jfoenix.controls.JFXListView;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import nju.lighting.blservice.documentblservice.DocBLService;
+import nju.lighting.presentation.factory.DocBLServiceFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created on 2018/1/1.
  * Description
  *
  * @author 陈俊宇
  */
-public class HomePage {
+public class HomePage  implements Initializable{
+    @FXML
+    JFXListView listView;
+
+    DocBLService blService= DocBLServiceFactory.getDocBLService();
+    
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }

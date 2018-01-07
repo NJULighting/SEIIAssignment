e9c91ebce00ccaccb9f4b430231974bb5f3f91d7
高梦婷 <161120030@smail.nju.edu.cn>
2018-01-07 11:05:12 +0800

    应收额度bug

--- Client/src/nju/lighting/presentation/utils/UserHelper.java
+++ Client/src/nju/lighting/presentation/utils/UserHelper.java
@@ -3,6 +3,7 @@ package nju.lighting.presentation.utils;
 import com.jfoenix.controls.JFXComboBox;
 import nju.lighting.presentation.factory.UserBLServiceFactory;
 import nju.lighting.vo.UserVO;
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 import java.util.stream.Collectors;
 
@@ -15,6 +16,14 @@ import java.util.stream.Collectors;
 public class UserHelper {
     public static UserVO getUser(String id) {
         return UserBLServiceFactory.getUserBLService().getUser(id);
+    }
+
+    public static String getUserString(String id){
+        UserVO userVO=getUser(id);
+        if (userVO==null)
+            return "";
+        else
+            return userVO.toString();
     }
 
     public static void  intiUserBox(JFXComboBox<UserVO> box){

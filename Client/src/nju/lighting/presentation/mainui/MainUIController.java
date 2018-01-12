0726dce7b22652cfa0aba26c789180e8adb2b1e6
高梦婷 <161120030@smail.nju.edu.cn>
2018-01-12 16:57:38 +0800

    删除一条println

--- Client/src/nju/lighting/presentation/mainui/MainUIController.java
+++ Client/src/nju/lighting/presentation/mainui/MainUIController.java
@@ -37,7 +37,6 @@ public abstract class MainUIController extends CommonFather {
         //设置下半部分界面
         if (center == null) {
             try {
-                System.out.println(url);
                 center = (Node) FXMLLoader.load(getClass().getClassLoader().getResource(url));
             } catch (IOException e) {
                 e.printStackTrace();
